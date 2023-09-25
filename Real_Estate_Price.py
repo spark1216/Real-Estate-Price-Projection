255546ab14e58f2c15b84249dc8a40aaad01c5bd
spark1216 <142547222+spark1216@users.noreply.github.com>
2023-09-24 17:47:01 -0700

    Update Real_Estate_Price.py

--- Real_Estate_Price.py
+++ Real_Estate_Price.py
@@ -5,7 +5,7 @@ st.title('US Real Estate Price Prediction')
 import util
 util.load_saved_artifacts()
 
-housesize= st.number_input('House Area (Square Feet)', 800)
+housesize= st.number_input('House Area (Square Feet)', 0)
 zipcode = st.number_input('Zipcode(5-Digit)',10021)
 acre_lot = st.number_input('Acre Lot (Square Feet)',0)
 bed = st.selectbox(
@@ -13,7 +13,7 @@ bed = st.selectbox(
     (0,1,2,3,4,5))
 bath = st.selectbox(
     'Bath',
-    (0,1,2,3,4,5))
+    (1,2,3,4,5))
 estimatepricebutton=st.button("Estimate Price!")
 if estimatepricebutton:
     price=util.get_estimated_price(bed, bath, acre_lot, housesize, zipcode)
